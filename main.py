8d7865c157220ec2a6966df536cd28067f6125bd
folaulau <folaukaveinga@gmail.com>
2021-03-08 11:32:11 -0700

    Update main.py

--- main.py
+++ main.py
@@ -35,3 +35,43 @@ if __name__ == '__main__':
     print(calculator.add(1,2))
     print(multiply(1, 2))
 
+    # set
+    numbers = set((1,2,3))
+    print(type(numbers))
+
+    numbers = {1, 2, 3}
+    print(type(numbers))
+
+    firstName = "Folau"
+    print(firstName)
+
+    firstName = str("Folau")
+    print(type(firstName))
+    print(firstName)
+
+    number = 2.5
+    print(number, "is float number?", isinstance(number, float))
+
+    lista = [1, 2.2, 'python']
+
+    print(lista)
+
+    # int
+    intNum = 2
+    print(type(intNum))
+    print(intNum)
+
+    #range
+    evenNumbers = range(2, 20, 2)
+    print(type(evenNumbers))
+    for num in evenNumbers:
+        print(num)
+
+    # dict
+    person = {"firstName":"Folau", "lastName":"Kaveinga"}
+    print(type(person))
+    print(person)
+
+    # Bool
+    isMale = True
+    print(isMale)
