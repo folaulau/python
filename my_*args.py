b4e102cd779d0c05e4aaec578a73c58e8863842f
folaulau <folaukaveinga@gmail.com>
2023-01-21 11:51:50 -0700

    Update my_*args.py

--- my_*args.py
+++ my_*args.py
@@ -12,7 +12,11 @@ def process_names(id: int, *args):
 process_names(1, "folau","kaveinga","jr")
 
 def process_levels(id: int, **args):
+    print("id:{}".format(id))
     for arg in args:
         print("arg name: {}, value={}".format(arg, args[arg]))
 
 process_levels(1, level1="folau", level2="kaveinga",level3="jr")
+
+print("\nno ** args\n")
+process_levels(1)
